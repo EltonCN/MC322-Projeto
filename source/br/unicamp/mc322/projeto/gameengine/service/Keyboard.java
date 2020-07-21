446f3e15b6a4290659e117598b26b668998781ba
Lucas-Otavio <50057427+Lucas-Otavio@users.noreply.github.com>
2020-07-21 18:47:59 -0300

    Pequenas mudanças no Keyboard

--- source/br/unicamp/mc322/projeto/gameengine/service/Keyboard.java
+++ source/br/unicamp/mc322/projeto/gameengine/service/Keyboard.java
@@ -1,9 +1,26 @@
 package br.unicamp.mc322.projeto.gameengine.service;
 
-import br.unicamp.mc322.projeto.gameengine.service.KeyInputService;
+import java.awt.event.KeyEvent;
 
-public class Keyboard
- implements KeyInputService
+public class Keyboard implements KeyInputService
 {
+
+	@Override
+	public void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPressed(KeyEvent key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isReleased(KeyEvent key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
 
