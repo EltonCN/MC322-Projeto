eeaaf0f25cb43926c73063d4ded941605c8e484a
Lucas-Otavio <50057427+Lucas-Otavio@users.noreply.github.com>
2020-07-24 17:54:08 -0300

    Consertei AttackableAreaEngine
    Extensão -> associação
    prevenção de exceções
    Adaptação e adição de métodos para reaproveitamento de código
    TRadução de comentários

--- source/br/unicamp/mc322/projeto/heroquest/component/AttackableRangeArea.java
+++ source/br/unicamp/mc322/projeto/heroquest/component/AttackableRangeArea.java
@@ -6,35 +6,53 @@ import br.unicamp.mc322.projeto.gameengine.pose.Metric;
 import br.unicamp.mc322.projeto.gameengine.pose.Pose;
 import br.unicamp.mc322.projeto.heroquest.entity.Attackable;
 import br.unicamp.mc322.projeto.heroquest.entity.Attacker;
-import br.unicamp.mc322.projeto.heroquest.entity.Creature;
 
 /**
  * Representa uma área de alcance de ataque
  */
-public class AttackableRangeArea extends EntityRangeArea {
+public class AttackableRangeArea {
 	
-	// In a few cases, it is intersting to have info about the entire stage's attackables
+	EntityRangeArea area;
+	
+	// Em alguns casos, é interessante acessar todos os atacáveis do estágio
 	public AttackableRangeArea() {
-		super(new Pose(0, 0, 0), Integer.MAX_VALUE, Metric.MANHATTAN);
+		area = new EntityRangeArea(new Pose(0, 0, 0), Integer.MAX_VALUE, Metric.MANHATTAN);
 	}
 	
 	public AttackableRangeArea(Pose origin, float range, Metric metric)
 	{
-		super(origin, range, metric);
+		area = new EntityRangeArea(origin, range, metric);
 	}
 
 	private void remove(Attackable[] at, int i) {
 		for (int j = i; j < at.length - 1; j++) {
+			if (at[j] == null)
+				break;
 			at[j] = at[j + 1];
 		}
 	}
 	
+	
+	/**
+	 * Retorna todos os atacáveis que não são do tipo do atacante, ie, todos que ele pode atacar
+	 * 
+	 * @todo implementar método inteiro
+	 */
 	public Attackable[] getAttackablesInside(Attacker attacker) {
+		return getAttackablesInside(!attacker.getIsFriendly()); // Previne aliados de se atacarem
+	}
+	
+	/**
+	 * Retorna todos os atacáveis que são do tipo de amigável dado
+	 * 
+	 * @todo implementar método inteiro
+	 */
+	public Attackable[] getAttackablesInside(boolean isFriendly) {
 		Attackable[] foes = getAttackablesInside();
 		for(int i = 0; i < foes.length; i++) 
 		{
-			if (attacker.getIsFriendly() == ((Attackable) foes[i]).getIsFriendly())
-				remove(foes, i--); // Preventing enemies/players attack themselves && the minus guarantees all Attackables are checked
+			if (isFriendly != ((Attackable) foes[i]).getIsFriendly())
+				remove(foes, i--); // O -- garante que todos os atacáveis serão visitados no for
 		}
 		
 		return foes;
@@ -42,27 +60,18 @@ public class AttackableRangeArea extends EntityRangeArea {
 	
 	private Attackable[] getAttackablesInside() {
 		
-		Entity[] entities = getEntitiesInside();
-		Attackable[] attackables = new Attackable[225]; //TODO CHANGE 225 TO A STAGE CONSTANT
+		Entity[] entities = area.getEntitiesInside();
+		Attackable[] attackables = new Attackable[225]; //TODO mudar 225 para uma cte de estágio
 		int added = 0;
 		
 		for(int i = 0; i < entities.length; i++) {
 			try {
 				attackables[added++] = (Attackable) entities[i];
 			} catch (ClassCastException e) {
-				added--;
+				added--; // O -- garante que todos os atacáveis serão visitados no for
 			}
 		}
 		return attackables;
-	}
-
-	/**
-	 * Retorna todos os atacáveis que são do tipo de amigável dado
-	 * 
-	 * @todo implementar método inteiro
-	 */
-	public Attackable[] getAttackablesInside(boolean friendlyType) {
-		return null;
 	}
 
 }
