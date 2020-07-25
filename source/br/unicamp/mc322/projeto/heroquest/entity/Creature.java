3c0064c2e7c5b81f602dcec38a9964b6321ddaad
Lucas-Otavio <50057427+Lucas-Otavio@users.noreply.github.com>
2020-07-25 17:24:50 -0300

    Inventário e controle de mãos atualizado

--- source/br/unicamp/mc322/projeto/heroquest/entity/Creature.java
+++ source/br/unicamp/mc322/projeto/heroquest/entity/Creature.java
@@ -11,7 +11,14 @@ import br.unicamp.mc322.projeto.heroquest.action.Movable;
 import br.unicamp.mc322.projeto.gameengine.action.ActionFailedException;
 import br.unicamp.mc322.projeto.gameengine.action.InvalidMovementException;
 import br.unicamp.mc322.projeto.gameengine.pose.Pose;
+import br.unicamp.mc322.projeto.gameengine.service.ServiceManager;
+import br.unicamp.mc322.projeto.gameengine.service.ServiceType;
 import br.unicamp.mc322.projeto.gameengine.service.entityrunner.RunnableTurn;
+import br.unicamp.mc322.projeto.gameengine.service.exception.DisabledServiceException;
+import br.unicamp.mc322.projeto.gameengine.service.exception.NotAvaibleServiceException;
+import br.unicamp.mc322.projeto.gameengine.service.log.LogPriority;
+import br.unicamp.mc322.projeto.gameengine.service.log.LogService;
+import br.unicamp.mc322.projeto.gameengine.service.log.LogType;
 
 public abstract class Creature extends HeroQuestEntity implements RunnableTurn, Attackable, Attacker, Movable {
     /** Attributes */
@@ -34,7 +41,7 @@ public abstract class Creature extends HeroQuestEntity implements RunnableTurn, 
     /**
      * Itens que possui equipados
      */
-    protected Item[] equippedItem;
+    protected Weapon[] equippedWeapons;
     /**
      * Armadura que a criatura está vestindo
      */
@@ -69,10 +76,7 @@ public abstract class Creature extends HeroQuestEntity implements RunnableTurn, 
     /**
      * Caster (faz magia) ou não
      */
-    protected boolean caster;
-    
-    private static final int INVENTORY_LIMIT = 10;
-    
+    protected boolean caster;    
     /**
      * Operation Creature
      *
@@ -90,7 +94,7 @@ public abstract class Creature extends HeroQuestEntity implements RunnableTurn, 
         usedHand = 0; // No início, nenhuma mão está sendo usada, assume-se
     	basicMovement = new NullMovement();
         turn = false;
-        equippedItem = new Item[10];
+        equippedWeapons = new Weapon[2];
         
     }
 
@@ -187,11 +191,34 @@ public abstract class Creature extends HeroQuestEntity implements RunnableTurn, 
      * Operation equipItem
      * Permite que Criatura equipe um item segundo as regras do jogo:
      * Dois itens de uma mão ou um item de uma duas mãos
-     * 
      */
     protected void equipWeapon(Weapon weapon) {
     	if (usedHand + weapon.getHands() < 3)
-    		equippedItem[usedHand++] = weapon;
+    		equippedWeapons[usedHand++] = weapon;
+    }
+    
+    /**
+     * Operation removeWeapon
+     * @param weapon
+     * @return Weapon
+     */
+    protected Weapon removeWeapon(int i) throws NullPointerException {
+    	if (equippedWeapons[i] != null) {
+    		Weapon toRemove = equippedWeapons[i];
+    		equippedWeapons[i] = null;
+    		usedHand -= toRemove.getHands();
+    		return toRemove;
+    	} else {
+    		try {
+				LogService l = (LogService) ServiceManager.getInstance().getService(ServiceType.LOG);
+				l.sendLog(LogType.OTHER, LogPriority.ERROR, "Creature", "Não foi possível descartar a arma, pois ela não foi encontrada.");
+			} catch (NotAvaibleServiceException | DisabledServiceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		
+    	}
+    	return null;
     }
 
     public void takeDamage(float damage) {
