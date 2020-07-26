f7935a46c47cb79387ed049a81435502e91a92cb
Lucas-Otavio <50057427+Lucas-Otavio@users.noreply.github.com>
2020-07-26 18:01:11 -0300

    Create Statue.java

---
+++ source/br/unicamp/mc322/projeto/heroquest/entity/Statue.java
@@ -0,0 +1,11 @@
+package br.unicamp.mc322.projeto.heroquest.entity;
+
+import br.unicamp.mc322.projeto.gameengine.pose.Pose;
+
+public class Statue extends Obstacle {
+
+	public Statue(Pose pose) {
+		super(pose);
+	}
+
+}
