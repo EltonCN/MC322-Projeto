cdca62d78e88f527796e4d4bbab5c9af364b6024
Lucas-Otavio <50057427+Lucas-Otavio@users.noreply.github.com>
2020-07-26 19:29:49 -0300

    Update RandomPose
    Agora é aleatória

--- source/br/unicamp/mc322/projeto/heroquest/item/Item.java
+++ source/br/unicamp/mc322/projeto/heroquest/item/Item.java
@@ -12,7 +12,6 @@ public abstract class Item
     /**
      * Nome do item
      */
-    @SuppressWarnings("unused")
 	private String name;
     /**
      * Operation Item
--- source/br/unicamp/mc322/projeto/heroquest/utility/RandomPose.java
+++ source/br/unicamp/mc322/projeto/heroquest/utility/RandomPose.java
@@ -1,13 +1,21 @@
 package br.unicamp.mc322.projeto.heroquest.utility;
 
 import br.unicamp.mc322.projeto.gameengine.pose.Pose;
+import br.unicamp.mc322.projeto.gameengine.utility.RandomGenerator;
 
 public class RandomPose 
 {
     public static Pose getResult()
     {
-        ///@todo implementar
-        return new Pose(100, 100);
+        ///@todo Adaptar Essa classe
+    	
+    	int passo = 48;
+    	int randomX = new RandomGenerator(15).getResult() * passo;
+    	int randomY = new RandomGenerator(8).getResult() * passo;
+    	
+    	
+    	
+        return new Pose(randomX, randomY);
     }
     
 }
