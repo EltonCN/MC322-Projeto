edec250fa53e64e62c67f0eeb47dd55d1689bd8d
Lucas-Otavio <50057427+Lucas-Otavio@users.noreply.github.com>
2020-07-26 19:12:52 -0300

    Update ChaseMovement.java

--- source/br/unicamp/mc322/projeto/heroquest/action/ChaseMovement.java
+++ source/br/unicamp/mc322/projeto/heroquest/action/ChaseMovement.java
@@ -5,6 +5,13 @@ import br.unicamp.mc322.projeto.gameengine.action.InvalidMovementException;
 import br.unicamp.mc322.projeto.gameengine.entity.DisabledEntityException;
 import br.unicamp.mc322.projeto.gameengine.entity.Entity;
 import br.unicamp.mc322.projeto.gameengine.pose.Metric;
+import br.unicamp.mc322.projeto.gameengine.service.ServiceManager;
+import br.unicamp.mc322.projeto.gameengine.service.ServiceType;
+import br.unicamp.mc322.projeto.gameengine.service.exception.DisabledServiceException;
+import br.unicamp.mc322.projeto.gameengine.service.exception.NotAvaibleServiceException;
+import br.unicamp.mc322.projeto.gameengine.service.log.LogPriority;
+import br.unicamp.mc322.projeto.gameengine.service.log.LogService;
+import br.unicamp.mc322.projeto.gameengine.service.log.LogType;
 import br.unicamp.mc322.projeto.heroquest.component.AttackableRangeArea;
 import br.unicamp.mc322.projeto.heroquest.entity.Attackable;
 import br.unicamp.mc322.projeto.heroquest.entity.Creature;
@@ -12,7 +19,7 @@ import br.unicamp.mc322.projeto.heroquest.entity.Movable;
 
 public class ChaseMovement implements Movement {
 	
-	protected Attackable chooseFoe(Creature mover, Attackable[] foes) {
+	protected Attackable chooseFoe(Creature mover, Attackable[] foes) throws ActionFailedException {
 		float minDistance = Float.MAX_VALUE;
 		Attackable theFoe = null;
 		float distance;
@@ -21,9 +28,12 @@ public class ChaseMovement implements Movement {
 			if (distance < minDistance) {
 				theFoe = foe;
 				minDistance = distance;
-				
 			}
 		}
+		
+		if (theFoe == null)
+			throw new ActionFailedException("Não há inimigos na tela para perseguir");
+		
 		return theFoe;
 	}
 	
@@ -33,10 +43,18 @@ public class ChaseMovement implements Movement {
 
 	@Override
 	public void move(Movable movable) throws ActionFailedException {
-		Attackable foe = chooseFoe((Creature) movable, getFoes((Creature) movable));
-		
-		if (foe == null) return;
-		
+		Attackable foe = null;
+		try {
+			foe = chooseFoe((Creature) movable, getFoes((Creature) movable));
+		} catch (ActionFailedException e) {
+			try {
+				LogService l = (LogService) ServiceManager.getInstance().getService(ServiceType.LOG);
+				l.sendLog(LogType.OTHER, LogPriority.LOG, "Chase Movement", "Chase Movement falhou: não há entidades atacáveis na tela");
+			} catch (NotAvaibleServiceException | DisabledServiceException e1) {
+				e1.printStackTrace();
+			}
+			
+		}
 		if (((Entity) movable).getPose().distance(((Entity) foe).getPose(), Metric.MANHATTAN) == 1)
 			return; // Movable pode atacar foe: não é necessário se mover
 		
